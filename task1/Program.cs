d03e8c448435776977600cd00079fbc3347f3ec8
Artemiy <birukovartemij@gmail.com>
2023-01-22 18:21:56 +0300

    task1 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ShowArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountOfPositiveNums(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0 ) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Давайте создадим Ваш массив: ");
+int [] arr = CreateArray();
+Console.Clear();
+Console.WriteLine("Ваш массив: ");
+ShowArray(arr);
+Console.WriteLine();
+Console.WriteLine($"В Вашем массиве {CountOfPositiveNums(arr)} положительных чисел.");
